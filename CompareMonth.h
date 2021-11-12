fc71481ee6045bfcf8ebfb159501da20bc7f2b22
turn.exe <80058678+typeplusplus@users.noreply.github.com>
2021-11-12 01:27:05 -0800

    Update CompareMonth.h

--- CompareMonth.h
+++ CompareMonth.h
@@ -10,6 +10,12 @@ using namespace std;
 class CompareMonth : public Person
 {
     public:
+        /*
+        operator: Comparative function for the base class member data comparing the Months
+        @param - Person pointer a
+        @param - Person pointer b
+        @return - none
+        */
         bool operator() (Person *a, Person *b);
 };
 

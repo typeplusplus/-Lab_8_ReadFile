6eb8f14eef73ab342d0fcae1c5d9d8454b29d5be
turn.exe <80058678+typeplusplus@users.noreply.github.com>
2021-11-12 07:46:59 -0800

    Update CompareYear.h

--- CompareYear.h
+++ CompareYear.h
@@ -10,6 +10,12 @@ using namespace std;
 class CompareYear : public Person
 {
     public:
+        /*
+        operator: Comparative function for the base class member data comparing the months
+        @param - Person pointer a
+        @param - Person pointer b
+        @return - none
+        */
         bool operator() (Person *a, Person *b);
 };
 

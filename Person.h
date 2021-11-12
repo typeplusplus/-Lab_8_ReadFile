cd82d272a0bb72b8d011e37de82a0cb6765129f0
turn.exe <80058678+typeplusplus@users.noreply.github.com>
2021-11-12 01:24:03 -0800

    Update Person.h

--- Person.h
+++ Person.h
@@ -9,17 +9,44 @@ using namespace std;
 class Person
 {
     public:
+            //Constructor
             Person();
             Person(string fn, string ln, string s, int m, int d, int y);
-
+            
+            //Member Functions
+            /***************************************/
+            /*
+            printInfo: Outputs information relatvent to the object used to call it
+            @param - None
+            @return - None
+            */
             void printInfo();
 
+            //Accessors
+            /***************************************/
+            /*
+            getMonth: Returns the Month value of the Person object
+            @param - None
+            @return - Month
+            */
             int getMonth();
+            
+            /*
+            getYear: Returns the Year value of the Person object
+            @param - None
+            @return - Month
+            */
             int getYear();
-
+    
+            /*
+            getMonth: Returns the Month value of the Person object
+            @param - None
+            @return - Month
+            */
             string getLastName();
 
     private:
+            //Member Data
             string fname, lname, gender;
             int month, day, year;
 };

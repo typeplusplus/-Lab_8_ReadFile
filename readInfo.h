514dc5c01dd3ec5c17f0ffd72ac9c3df3f91fe66
turn.exe <80058678+typeplusplus@users.noreply.github.com>
2021-11-13 21:14:26 -0800

    Update readInfo.h

--- readInfo.h
+++ readInfo.h
@@ -8,6 +8,17 @@
 
 using namespace std;
 
+/*
+Function Name: readInfo
+Purpose: This program receives the reference vector and filename, the file will be opened and data will be fed to the declared varibles. Objects will be created for each grouping of data passed from the files. Created objects are then pushed into the reference vector passed to this function.
+
+@author Reece Doyle
+
+@param - filename: Name of the file to be used for input stream
+@param - &fptr: Vector of pointers for the objects to be pushed placed in
+@return - none
+*/
+
 void readInfo(string filename, vector<Person *> &fptr);
 
 #endif

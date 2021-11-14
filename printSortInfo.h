b54e1f1309eb872ca7ec4ffdcab58c85e72a64a4
turn.exe <80058678+typeplusplus@users.noreply.github.com>
2021-11-13 20:57:18 -0800

    Update printSortInfo.h

--- printSortInfo.h
+++ printSortInfo.h
@@ -12,6 +12,17 @@
 
 using namespace std;
 
+/*
+Function Name: printSortInfo
+Purpose: This function takes a vector of pointers and sorts it based on the integer flag that is also passed to the function. 
+ 
+@author Reece Doyle
+
+@param - &fptr: Vector containing pointers of the 
+@param - flag: Integer to determine what sorting method is used for the vector
+@return - none
+*/
+
 void printSortInfo(vector<Person *> &fptr, int flag);
 
 #endif
